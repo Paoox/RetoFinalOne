3749b75299df286e508966e5c868c435767d048d
Paoox <pao.arreola.g@gmail.com>
2024-07-13 20:27:58 -0600

    first commit

---
+++ src/test/java/com/api_forHub/retoFinalOne/RetoFinalOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_forHub.retoFinalOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoFinalOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
